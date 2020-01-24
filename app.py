3a5fb32b8951658a318d568955351e3acf94de8c
leslitagordita <contact@lesliesalazar.com>
2020-01-24 16:25:12 -0500

    Final project structure

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-import re
-from flask import Flask, redirect, request, render_template, session, send_from_directory
+from flask import Flask, request, render_template
 from flask_session import Session
 from geojson import Point
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
+import json
 
 app=Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/linodeStreetTrees2"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/linodeStreetTrees"
 mongo = PyMongo(app)
 
 @app.route('/')
-def main():
-    street_trees_points = dumps(mongo.db.linodeStreetTrees2.find({}, {'_id': False}))
-    return render_template('base_2.html', street_trees_points=street_trees_points)
+def index():
+    street_trees_points_query = dumps(mongo.db.linodeStreetTrees.find({}, {'_id': False}))
+    street_trees_points = json.loads(street_trees_points_o)
+    return render_template('base.html', street_trees_points=street_trees_points)
 
 if __name__ == '__main__':
     app.debug=True
